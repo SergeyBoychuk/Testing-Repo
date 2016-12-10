25cf3df5603265850bc03a341887e6b8d31aec86
Sergey Boychuk <sergeboychuk@gmail.com>
2016-12-10 14:33:31 -0500

    Initial Commit

---
+++ app/js/home.js
@@ -0,0 +1,14 @@
+var count = 0,
+    wordsArray = [
+        "Voted best coffee shop in Palm Beach County!",
+        "Come visit us soon and we'll give you a discount!",
+        "This is a limited to offer so come soon!",
+        "We welcome you to come visit us",
+    ];
+setInterval(function () {
+    count+= 1;
+    isFired = 1;
+    $(".test").fadeOut(400, function () {
+        $(this).text(wordsArray[count % wordsArray.length]).fadeIn(400);
+  });
+}, 5000);
